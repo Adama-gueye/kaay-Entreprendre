23fb6f2be7bceb350d2974998d808a1c3ff9259f
Adama Gueye <adamarahma99@gmail.com>
2023-12-07 14:10:59 +0000

    enrigstrement pour faire un checkout

---
+++ database/migrations/2023_12_07_135540_create_livrables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livrables', function (Blueprint $table) {
+            $table->id();
+            $table->string('contenu');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('ressource_id');
+            $table->foreign('ressource_id')->references('id')->on('ressources')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livrables');
+    }
+};
